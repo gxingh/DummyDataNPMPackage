02bb4e90027b2db9eb4184b9edcaeadccd78c1a7
Gagandeep Singh <gagandeep.singh@Kony.com>
2018-12-11 15:47:08 +0530

    further function added

--- demo/main.js
+++ demo/main.js
@@ -1,3 +1,8 @@
 const dummy = require("@iamtherealgd/dummydata");
 
 console.log(dummy.getString());
+
+var list = dummy.getListString(5);
+for(var i=0; i<list.length; i++){
+	console.log(list[i]);
+}
